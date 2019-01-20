03ffcd4ee29f84a53a09e61258f526990d6c65ff
Bruce 🛳  Gou <brucegou@amazon.com>
2019-01-20 00:05:29 -0800

    updated loading prompt

--- pages/index/index.js
+++ pages/index/index.js
@@ -30,12 +30,6 @@ Page({
   bindDeliveryLocationChange: function(e) {
     this.setData({selectedLocationIndex: e.detail.value})
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
